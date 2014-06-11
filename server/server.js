0f356cb48528f3cc0cfe1f4621e6d69f69e585a1
Chase West <chaserit@gmail.com>
2014-06-10 22:22:03 -0700

    removing server.stop from server.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,11 @@ var Hapi = require('hapi'),
     swig = require('swig'),
     routes = require("./routes/routes.js"),
     options = require("./config/config.js");
-    
+
 var server = new Hapi.Server('localhost', 8000, options);
 
 server.route(routes);
 
 server.start(function () {
     console.log('Server started at: ' + server.info.uri);
-});
-
-server.stop({ timeout: 60 * 1000 }, function () {
-    console.log('Server stopped');
 });
